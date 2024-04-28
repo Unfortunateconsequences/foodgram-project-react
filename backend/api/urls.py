830af3d82beedbd638c709d680e0ee188666c5b9
Unfortunateconsequences <142180496+Unfortunateconsequences@users.noreply.github.com>
2024-04-28 21:13:00 +0200

    major changes

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path
+from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from api.views import TagViewSet, IngredientViewSet, RecipeViewSet
+from api.views import (
+    TagViewSet,
+    IngredientViewSet,
+    RecipeViewSet,
+    UserViewSet
+)
 
 
 app_name = 'api'
@@ -9,12 +14,15 @@ router = DefaultRouter()
 router.register('tags', TagViewSet, basename='tag')
 router.register('ingredients', IngredientViewSet, basename='ingredient')
 router.register('recipes', RecipeViewSet, basename='recipe')
+router.register('users', UserViewSet, basename='users')
 
 urlpatterns = [
+    path('', include(router.urls)),
+    path('', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
     path(
         'recipes/download_shopping_list/',
         RecipeViewSet.as_view({'get': 'download_shopping_cart'}),
         name='download_shopping_cart',
     )
 ]
-urlpatterns += router.urls
